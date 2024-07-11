7a09cfc2167c2b6a7ce1d5b8c56b287bbcbb9bc9
Abu Talha <98013540+themaker8@users.noreply.github.com>
2024-07-11 21:23:25 +0530

    Update page.js

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -4,6 +4,8 @@ import { useState, useEffect } from 'react';
 import { collection, onSnapshot, updateDoc, doc, increment } from 'firebase/firestore';
 import { defaultDb, auth } from '../firebase/config';
 import Navbar from '../navbar';
+import { signOut } from 'firebase/auth';
+import { useRouter } from 'next/navigation';
 
 export default function Skills() {
   const [skills, setSkills] = useState([]);
@@ -11,6 +13,7 @@ export default function Skills() {
   const [user, setUser] = useState(null);
   const [totalScore, setTotalScore] = useState(0);
   const [level, setLevel] = useState(1);
+  const router = useRouter();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(currentUser => {
@@ -19,12 +22,22 @@ export default function Skills() {
         fetchSkills(currentUser.uid);
         fetchUserData(currentUser.uid);
       } else {
+        setUser(null);
         console.error('User not authenticated');
       }
     });
 
     return () => unsubscribe();
   }, []);
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+      router.push('/'); // Redirect to the homepage after logout
+    } catch (error) {
+      console.error('Error logging out:', error);
+    }
+  };
 
   const fetchSkills = async (userId) => {
     try {
@@ -96,68 +109,85 @@ export default function Skills() {
 
   return (
     <div className="min-h-screen flex flex-col bg-gray-900 text-white pb-16">
-      <div className="p-4 md:p-6 lg:p-8 max-w-3xl mx-auto flex-grow">
-        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center">
-          {user ? `Welcome, ${user.displayName}` : 'Welcome to Your RPG Adventure!'}
-        </h1>
+      {user ? (
+        <>
+          <div className="p-4 md:p-6 lg:p-8 max-w-3xl mx-auto flex-grow">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-6 text-center">
+              Welcome, {user.displayName}
+            </h1>
 
-        <div className="mb-8">
-          <h2 className="text-lg font-semibold mb-2 text-center">Total Score</h2>
-          <div className="relative w-full bg-gray-700 p-4 rounded-lg">
-            <div className="relative h-6 bg-gray-600 rounded-lg overflow-hidden">
-              <div
-                className="absolute h-6 bg-green-400 rounded-lg"
-                style={{ width: `${Math.min(totalScore, 20000) / 20000 * 100}%`, transition: 'width 0.5s ease-in-out' }}
-              />
+            <div className="mb-8 flex justify-center">
+              <button
+                onClick={handleLogout}
+                className="bg-red-500 text-white py-2 px-4 rounded-lg"
+              >
+                Logout
+              </button>
             </div>
-            <p className="text-center text-xl font-bold mt-2">Total Score: {totalScore}</p>
-          </div>
-        </div>
 
-        <div className="mb-8">
-          <h2 className="text-lg font-semibold mb-2 text-center">Your Level</h2>
-          <div className="text-center text-xl font-bold mb-4">
-            Level: {level}
-          </div>
-          <div className="relative w-full bg-gray-700 p-2 rounded-lg">
-            <div className="relative h-2 bg-gray-600 rounded-lg overflow-hidden">
-              <div
-                className="absolute h-2 bg-blue-400 rounded-lg"
-                style={{ width: `${getLevelProgressPercentage(totalScore)}%`, transition: 'width 0.5s ease-in-out' }}
-              />
+            <div className="mb-8">
+              <h2 className="text-lg font-semibold mb-2 text-center">Total Score</h2>
+              <div className="relative w-full bg-gray-700 p-4 rounded-lg">
+                <div className="relative h-6 bg-gray-600 rounded-lg overflow-hidden">
+                  <div
+                    className="absolute h-6 bg-green-400 rounded-lg"
+                    style={{ width: `${Math.min(totalScore, 20000) / 20000 * 100}%`, transition: 'width 0.5s ease-in-out' }}
+                  />
+                </div>
+                <p className="text-center text-xl font-bold mt-2">Total Score: {totalScore}</p>
+              </div>
             </div>
-            <p className="text-center text-sm font-semibold mt-1">Score to next level: {20000 - (totalScore % 20000)} points</p>
-          </div>
-        </div>
 
-        <div>
-          <h3 className="text-xl font-semibold mb-4 text-center">Your Skills</h3>
-          <ul className="space-y-4">
-            {skills.map((skill) => (
-              <li key={skill.id} className="flex items-center space-x-4 bg-gray-800 p-4 rounded-lg shadow-md">
-                <div className="flex-1">
-                  <span className="font-bold text-xl">{skill.name}</span>
+            <div className="mb-8">
+              <h2 className="text-lg font-semibold mb-2 text-center">Your Level</h2>
+              <div className="text-center text-xl font-bold mb-4">
+                Level: {level}
+              </div>
+              <div className="relative w-full bg-gray-700 p-2 rounded-lg">
+                <div className="relative h-2 bg-gray-600 rounded-lg overflow-hidden">
+                  <div
+                    className="absolute h-2 bg-blue-400 rounded-lg"
+                    style={{ width: `${getLevelProgressPercentage(totalScore)}%`, transition: 'width 0.5s ease-in-out' }}
+                  />
                 </div>
-                <button 
-                  onClick={() => increaseScore(skill.id)}
-                  className="bg-green-500 text-white p-2 rounded-lg hover:bg-green-600"
-                >
-                  +
-                </button>
-                <div className="w-1/2">
-                  <div className={`relative h-2 rounded-lg ${getRandomSkillBarColor()}`}>
-                    <div
-                      className={`absolute h-2 rounded-lg ${getRandomSkillBarColor()}`}
-                      style={{ width: `${Math.min(skill.score, 100)}%`, transition: 'width 0.5s ease-in-out' }}
-                    />
-                  </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                <p className="text-center text-sm font-semibold mt-1">Score to next level: {20000 - (totalScore % 20000)} points</p>
+              </div>
+            </div>
+
+            <div>
+              <h3 className="text-xl font-semibold mb-4 text-center">Your Skills</h3>
+              <ul className="space-y-4">
+                {skills.map((skill) => (
+                  <li key={skill.id} className="flex items-center space-x-4 bg-gray-800 p-4 rounded-lg shadow-md">
+                    <div className="flex-1">
+                      <span className="font-bold text-xl">{skill.name}</span>
+                    </div>
+                    <button 
+                      onClick={() => increaseScore(skill.id)}
+                      className="bg-green-500 text-white p-2 rounded-lg hover:bg-green-600"
+                    >
+                      +
+                    </button>
+                    <div className="w-1/2">
+                      <div className={`relative h-2 rounded-lg ${getRandomSkillBarColor()}`}>
+                        <div
+                          className={`absolute h-2 rounded-lg ${getRandomSkillBarColor()}`}
+                          style={{ width: `${Math.min(skill.score, 100)}%`, transition: 'width 0.5s ease-in-out' }}
+                        />
+                      </div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <Navbar /> {/* Include the Navbar component */}
+        </>
+      ) : (
+        <div className="flex justify-center items-center h-full">
+          <p className="text-xl">User not authenticated. Please log in.</p>
         </div>
-      </div>
-      <Navbar /> {/* Include the Navbar component */}
+      )}
     </div>
   );
 }
